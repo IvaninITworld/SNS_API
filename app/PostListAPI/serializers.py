c51542a231d2541d4e23a5bf7c943eda06e8fdab
Ivan Kim <xormrdlsrks2@gmail.com>
2023-11-09 17:22:06 +0900

    [REV] Transition

--- app/PostListAPI/serializers.py
+++ app/PostListAPI/serializers.py
@@ -4,18 +4,42 @@ from django.contrib.auth.models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
+    # define a new field
+    is_following = serializers.SerializerMethodField()
+
     class Meta:
         model = User
         fields = (
             "id",
             "username",
             "password",
+            "is_following",
         )
 
         extra_kwargs = {"password": {"write_only": True}}
 
+    # get_<field_name>()
+    def get_is_following(self, obj):
+        request_user = self.context["request"].user
+        return Follow.objects.filter(follower=request_user, following=obj).exists()
+
 
 class PostSerializer(serializers.ModelSerializer):
+    username = serializers.ReadOnlyField(source="user.username")
+
+    class Meta:
+        model = Post
+        fields = "__all__"
+        read_only_fields = (
+            "id",
+            "user",
+            "username",
+            "created_date",
+            "updated_date",
+        )
+
+
+class PostUploadSerializer(serializers.ModelSerializer):
     username = serializers.ReadOnlyField(source="user.username")
 
     class Meta:
@@ -25,9 +49,19 @@ class PostSerializer(serializers.ModelSerializer):
             "user",
             "username",
             "content",
+            "image",
             "created_date",
             "updated_date",
         )
+        read_only_fields = (
+            "id",
+            "user",
+            "username",
+            "created_date",
+            "updated_date",
+        )
+
+    image = serializers.ImageField(required=False)
 
 
 class FollowSerializer(serializers.ModelSerializer):
